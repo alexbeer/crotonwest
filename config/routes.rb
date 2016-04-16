a8aedcb3785e022b725c835598976bc1bed736d5
Alex Beer <alexander.b.beer@gmail.com>
2016-04-16 11:51:42 -0500

    trying to sync

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get "stories" => "pages#stories"
 
   get "guides" => "pages#guides"
+
+  get "guideexample" => "pages#guideexample"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
