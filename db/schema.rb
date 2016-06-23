9ada1192295a9c499242e80857d72bf23a0dc385
Max Kiss <maxkiss2@gmail.com>
2016-06-23 15:54:24 -0400

    heroku testz

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615201225) do
+ActiveRecord::Schema.define(version: 20160623165218) do
 
   create_table "galleries", force: :cascade do |t|
     t.string   "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160615201225) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "title"
   end
 
   add_index "galleries", ["user_id"], name: "index_galleries_on_user_id"
