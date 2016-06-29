b83e740fe1bb62bf401d65450055157c9e5e957e
Max Kiss <maxkiss2@gmail.com>
2016-06-29 16:50:44 -0400

    update gallery class

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,7 @@
 class Gallery < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },       
+:default_url => "missing_:style.png", :default_url => 'missing_:style.png', :storage =>   
+:s3, :bucket => "<my_bucket>"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
