97bfda22b8aab57c88f48b15e60af77ade363c29
Max Kiss <maxkiss2@gmail.com>
2016-06-29 15:10:18 -0400

    form update s3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623165218) do
+ActiveRecord::Schema.define(version: 20160629185610) do
 
   create_table "galleries", force: :cascade do |t|
     t.string   "description"
