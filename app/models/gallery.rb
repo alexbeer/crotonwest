df494e443697741c5964c09a34c056ed05d7febc
Max Kiss <maxkiss2@gmail.com>
2016-06-29 16:55:01 -0400

    Gallery image instance update

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,13 @@
 class Gallery < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },       
-:default_url => "missing_:style.png", :default_url => 'missing_:style.png', :storage =>   
-:s3, :bucket => "<my_bucket>"
-  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	has_many :images, class_name: 'GalleryImage', dependent: :destroy
+	accepts_nested_attributes_for :images, :allow_destroy => true
+
+ 	 default_scope -> { order('created_at DESC') }
+  	validates :user_id, presence: true
+
+	has_attached_file :image, :styles => { :large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }
+
+	validates :title, presence: true
 end
