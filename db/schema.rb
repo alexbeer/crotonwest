562e7f465818b8fe350d00e4e81604a952557b3a
alex beer <alexander.b.beer@gmail.com>
2016-06-30 15:15:27 -0400

    fixed divs on stories and guides page

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160629185610) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "title"
   end
 
   add_index "galleries", ["user_id"], name: "index_galleries_on_user_id"
