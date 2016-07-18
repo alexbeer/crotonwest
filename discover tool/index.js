4bb148d6f25035968d0fce298342b22c28508881
alex beer <alexander.b.beer@gmail.com>
2016-07-17 23:42:23 -0400

    newest discover tool

--- discover tool/index.js
+++ discover tool/index.js
@@ -1,4 +1,48 @@
 
+$("#next_button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#second_whole_page").offset().top
+    }, 1000);
+});
+
+$("#view_park_next_button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#your_park").offset().top
+    }, 1000);
+});
+
+
+// beta modal stuff
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
 
   var byProperty = [], byColor = [], byLocation = [];
 
